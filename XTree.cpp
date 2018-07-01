c35b755fb3d156f8bdaf4ed2b6430dccf0bbe1c4
Rodrigo Gutierrez <rodrigo.gutierrez.k@gmail.com>
2018-07-01 18:08:48 -0500

    atributos y metodos inicales, adaptando el rtree a multidimensional

--- XTree.cpp
+++ XTree.cpp
@@ -2,9 +2,49 @@
 #include "XTree.h"
 
 
+XTree::XTree(int Dimensiones) {
+    Dimensions = Dimensiones;
+}
+
 XTree::XTree() {
 }
 
 
 XTree::~XTree() {
+}
+
+Nodo * XTree::ChooseLeaf(Nodo * Data) {
+    return nullptr;
+}
+
+Nodo * XTree::SplitNodo(Nodo * nodo) {
+    return nullptr;
+}
+
+void XTree::Insert(vector<double> point) {
+}
+
+void XTree::AdjustTree(Nodo * N, Nodo * NN) {
+}
+
+vector<int> XTree::PickSeeds(vector<Nodo*>& Entradas) {
+    return vector<int>();
+}
+
+int XTree::PickNext(vector<Nodo*>& Entradas, Nodo * G1, Nodo * G2) {
+    return 0;
+}
+
+void XTree::ComponerRegion(vector<double> R1LU, vector<double> R1RB, vector<double> R2LU, vector<double> R2RB, vector<double>& RLU, vector<double>& RRB) {
+}
+
+float XTree::AreaRegion(vector<double> LU, vector<double> RB) {
+    return 0.0f;
+}
+
+void XTree::Imprimir() {
+}
+
+string XTree::Identacion(int Tam) {
+    return string();
 }
--- XTree.h
+++ XTree.h
@@ -1,7 +1,30 @@
 #pragma once
 
+#include<iostream>
+#include<vector>
+#include<string>
+#include"Nodo.h"
+
+using namespace std;
+
 class XTree {
     public:
+        int Dimensions;
+        int M;
+        int m; // m <= M/2
+        Nodo * Root;
+        Nodo * ChooseLeaf(Nodo * Data);
+        Nodo * SplitNodo(Nodo * nodo);
+        void Insert(vector<double> point);//quiza deberi recibir un nodo data point
+        void AdjustTree(Nodo * N, Nodo * NN);
+        vector<int> PickSeeds(vector<Nodo *> & Entradas);
+        int PickNext(vector<Nodo *> & Entradas, Nodo * G1, Nodo * G2);
+        void ComponerRegion(vector<double> R1LU, vector<double> R1RB, vector<double> R2LU, vector<double> R2RB, vector<double> & RLU, vector<double> & RRB);
+        float AreaRegion(vector<double> LU, vector<double> RB);//deberia tener usar la clase region
+        void Imprimir();
+        string Identacion(int Tam);
+
+        XTree(int Dimensiones);
         XTree();
         ~XTree();
 };

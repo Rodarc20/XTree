052836931c2817cc7e17fce103f122d5601d2e2f
Rodrigo Gutierrez <rodrigo.gutierrez.k@gmail.com>
2018-07-01 16:42:31 -0500

    atributos y cabecedaras de metodos para la clase nodo

--- Nodo.cpp
+++ Nodo.cpp
@@ -8,3 +8,27 @@ Nodo::Nodo() {
 
 Nodo::~Nodo() {
 }
+
+float Nodo::CoverageArea() {
+    return 0.0f;
+}
+
+void Nodo::CalcularCoverage() {
+}
+
+bool Nodo::Pertenece(vector<float> p) {
+    return false;
+}
+
+void Nodo::AddHijo(Nodo * hijo) {
+}
+
+void Nodo::Imprimir(int iden) {
+}
+
+string Nodo::Identacion(int Tam) {
+    return string();
+}
+
+Nodo::Nodo(vector<float> p) {
+}
--- Nodo.h
+++ Nodo.h
@@ -1,7 +1,26 @@
 #pragma once
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
 
 class Nodo {
     public:
+        vector<float> PointLU;
+        vector<float> PointRB;
+        Nodo * Padre;
+        vector<Nodo *> Hijos;
+        vector<float> DataPoint;
+        bool bDataPoint;
+        bool bHoja;//si es hoja, tiene un DataPoint, es decir apunt hacia algo un registro o algo, si no es hoja quiere decir que tiene hijos
+        float CoverageArea();
+        void CalcularCoverage();
+        bool Pertenece(vector<float> p);
+        void AddHijo(Nodo * hijo);
+        void Imprimir(int iden);
+        string Identacion(int Tam);
+        Nodo(vector<float> p);
         Nodo();
         ~Nodo();
 };
